07849df28c7d753759d6ad593916412812dae68e
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-03 14:29:55 +0100

    Install and import swagger-jsdoc swagger-ui-express express nodemon packages, set up server

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+
+const app = express();
+
+app.listen(5000, () => console.log("listening on 5000"));
