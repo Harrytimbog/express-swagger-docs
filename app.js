b0cb8a5d5f7894f429f321739ddb9ea344f9e72f
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-03 14:55:42 +0100

    Integrate swagger UI and test for drones get request

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express');
-const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
 const app = express();
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Library API',
+      version: '1.0.0'
+    }
+  },
+  apis: ['app.js'],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+// console.log(swaggerDocs);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /drones:
+ *   get:
+ *      description: Get all Drones
+ *      responses:
+ *        200:
+ *          description: Success
+ *
+  */
 
 app.get('/drones', (req, res) => {
   res.send([
