2f568896a498a6d24eddf08854630e1a42a070c3
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-03 14:37:04 +0100

    Create drones API path

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
-
 const app = express();
+
+app.get('/drones', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      model: 'Holystone HS240'
+    },
+
+    {
+      id: 2,
+      model: 'Holystone HS160'
+    }
+  ])
+});
+
 
 app.listen(5000, () => console.log("listening on 5000"));
